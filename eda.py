99c26f51fdeb7e34cae1a5804858c4d365c9af0f
grazel98 <38445776+grazel98@users.noreply.github.com>
2019-08-29 12:50:00 -0700

    eda comments

--- eda.py
+++ eda.py
@@ -3,12 +3,19 @@ import csv
 
 abrv = 'CHI'
 
-chi_weather_data = pd.read_csv(f'{abrv}_weather_data.csv')
+chi_weather_data = pd.read_csv(f'{abrv}_weather_data.csv', index_col='date', parse_dates=True)
 
-chi_weather_data.drop(['avg_hum','avg_wind','avg_pres','hi_precip','lo_precip'],
-		axis=1, inplace=True)
+chi_ridership_data = pd.read_csv('CTA_Daily_Boarding_Totals.csv', index_col='service_date', parse_dates=True)
 
-chi_weather_data.hist(bins=10)
+chi_data = chi_ridership_data.join(chi_weather_data)
 
-# print(list(chi_weather_data))#
-# print(chi_weather_data.info())
+# drop empty columns
+chi_data.drop(['avg_hum','avg_wind','avg_pres','hi_precip','lo_precip'], axis=1, inplace=True)
+
+# drop days with erroneous data; happens to be last two data points
+chi_data.drop(chi_data['2018-06-29':].index, axis=0, inplace=True)
+
+# check distributions of features using histograms
+pd.DataFrame.hist(data=chi_data, figsize=(15,15), layout=(4,4), bins=15);
+
+
